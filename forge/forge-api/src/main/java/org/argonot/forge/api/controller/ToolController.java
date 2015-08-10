9ed588c2cd5e649849dd3deb6755d70437029d25
Meidi Airouche <meidi.airouche@gmail.com>
2015-08-11 01:48:14 +0200

    Revert "Refactoring : extract commons utils and filters"
    This reverts commit 6ad07662443e37816280b78984197f94ac55fd8c.

--- forge/forge-api/src/main/java/org/argonot/forge/api/controller/ToolController.java
+++ forge/forge-api/src/main/java/org/argonot/forge/api/controller/ToolController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  *
  */
 @Controller
-@RequestMapping("/tools")
+@RequestMapping("/tool")
 public class ToolController {
 
     @Autowired
